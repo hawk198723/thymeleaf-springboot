8259815f66267565a2d8a9494dd102ea83b6695b
hawk198723 <hawk198723@gmail.com>
2022-12-16 23:25:57 -0800

    Initial commit

---
+++ src/main/java/com/jason/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.jason.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/test")
+    public String test(Model model){
+        model.addAttribute("msg","wtf,springboot");
+        return "test";
+    }
+}
