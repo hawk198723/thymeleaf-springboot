ba3894e7e9dda95e84a1189c2fc030a870a05f46
hawk198723 <hawk198723@gmail.com>
2022-12-19 16:36:45 -0800

    mvc

--- src/main/java/com/jason/controller/HelloController.java
+++ src/main/java/com/jason/controller/HelloController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @Controller
 public class HelloController {
     @RequestMapping("/test")
     public String test(Model model){
-        model.addAttribute("msg","wtf,springboot");
+        model.addAttribute("msg","<h1>wtf,spring boot thymeleaf</h1>");
+        model.addAttribute("users", Arrays.asList("wow","hahah","Lol","wth"));
         return "test";
     }
 }
